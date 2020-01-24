10c135e06d4bb1f5d85d569a7cb9fe6e7740bc2d
NadeemAhmedd <aknadeemahmed@gmail.com>
2020-01-24 18:53:22 +0500

    Tidy up the views

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">
                    <div class="d-flex align-items-center">
@@ -18,53 +18,14 @@
                 <div class="card-body">
                     @include('layouts._messages')
                     @forelse($questions as $question)
-                        <div class="media">
-                            <div class="d-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{$question->votes_count}}</strong> {{Str::plural('Vote', $question->votes_count)}}
-                                </div>
-                                <div class="status {{ $question->status }}">
-                                    <strong>{{$question->answers_count}}</strong> {{Str::plural('Answer', $question->answers_count)}}
-                                </div>
-                                <div class="view">
-                                    {{$question->views .' '. Str::plural('View', $question->views)}}
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a></h3>
-                                    <div class="ml-auto">
-                                        @can('update', $question)
-                                            <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info"> Edit </a>
-                                        @endcan
-                                        @can('delete', $question)
-                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                            @method("DELETE")
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure?')"> Delete </button>
-                                        </form>
-                                        @endcan
-                                    </div>
-                                </div>
-                                <div class="lead">
-                                    Asked by 
-                                    <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                    <small class="text-muted">
-                                        {{$question->created_date}}
-                                    </small>
-                                </div>
-                                {{ $question->excerpt(250)}}
-                            </div>
-                        </div>
-                        <hr>
+                        @include('questions._question')
                     @empty
                         <div class="alert alert-warning">
                             <strong>Sorry</strong> THere are no questions available
                         </div>
                     @endforelse
-                    <div class="max-auto">
-                        {{$questions->links()}}
-                    </div>
+                    
+                    {{$questions->links()}}
                 </div>
             </div>
         </div>
